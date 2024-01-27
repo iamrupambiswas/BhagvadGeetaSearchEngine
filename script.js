2af1e931cd30e6787abcf7f420c7ef925ca1da70
Rupam Biswas <rupambiswas953@gmail.com>
2024-01-28 01:18:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const url = 'https://bhagavad-gita3.p.rapidapi.com/v2/chapters/?limit=18';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a02f5b90f3mshc436d615efbcb7ap1c470cjsnae3960d36b68',
+        'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
+	}
+};
+
+window.onload = () => {
+    get();
+};
+
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13) {
+      get();
+    }
+});
+
+
+function get(){
+    const chapter = document.getElementById('chapter').value;
+    const id = parseInt(chapter) - 1;
+    const output = document.querySelector('#nameOutput');
+    const summaryOutput = document.querySelector('#summary');
+    const meaningOutput = document.querySelector('#meaning');
+    
+    if (chapter < 1 || chapter > 18){
+        alert("There are 1 to 18 chapters in Bhagvad Gita, select between the range!");
+    }
+
+    try {
+        (async () => {
+            const response = await fetch(url, options);
+            const result = await response.json();
+
+            const chapterName = result[id].name;
+            output.innerText = chapterName;
+
+            const summary = result[id].chapter_summary;
+            summaryOutput.innerText = summary;
+
+            const meaning = result[id].name_meaning;
+            meaningOutput.innerText = meaning;
+        })();
+    } 
+    catch (error) {
+        console.error(error);
+    }
+    
+}
